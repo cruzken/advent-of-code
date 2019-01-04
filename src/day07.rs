5b38732bb4378fd05d7177ebbf61598c07291c77
Ken Cruz <ken@kencruz.ca>
2019-01-03 21:24:22 -0500

    added day07::star_one example input

--- src/day07.rs
+++ src/day07.rs
@@ -16,7 +16,13 @@ mod tests {
 
     #[test]
     fn test_star_one() {
-        assert_eq!(star_one(""), 1)
+        assert_eq!(star_one("Step C must be finished before step A can begin.
+Step C must be finished before step F can begin.
+Step A must be finished before step B can begin.
+Step A must be finished before step D can begin.
+Step B must be finished before step E can begin.
+Step D must be finished before step E can begin.
+Step F must be finished before step E can begin."), 1)
     }
 
     #[test]
