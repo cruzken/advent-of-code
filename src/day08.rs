5fe2df6337d903b0725d653407b5eec819aed792
Ken Cruz <ken@kencruz.ca>
2019-01-22 03:17:13 -0500

    day08::star_one parsed input to data

--- src/day08.rs
+++ src/day08.rs
@@ -1,6 +1,11 @@
 #[allow(dead_code)]
 #[allow(unused_variables)]
 pub fn star_one(input: &str) -> i64 {
+    let data: Vec<u32> = input
+        .split_whitespace()
+        .map(|x| x.parse().unwrap())
+        .collect();
+    println!("{:?}", data);
     0
 }
 
@@ -16,7 +21,7 @@ mod tests {
 
     #[test]
     fn test_star_one() {
-        assert_eq!(star_one(""), 1)
+        assert_eq!(star_one("2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2"), 1)
     }
 
     #[test]
