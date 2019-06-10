7fc2acac96f2a50566c0a5fee74ead6394c49fa5
Ken Cruz <ken@kencruz.ca>
2019-06-10 12:09:25 -0400

    added day12 initial state parser

--- src/day12.rs
+++ src/day12.rs
@@ -1,6 +1,7 @@
 #[allow(dead_code)]
 #[allow(unused_variables)]
 pub fn star_one(input: &str) -> i64 {
+    let state = initial_state(input);
     0
 }
 
@@ -10,13 +11,19 @@ pub fn star_two(input: &str) -> i64 {
     0
 }
 
+fn initial_state(input: &str) -> String {
+    input.lines().nth(0).unwrap().replace("initial state: ", "")
+}
+
 #[cfg(test)]
 mod tests {
     use super::{star_one, star_two};
 
     #[test]
     fn test_star_one() {
-        assert_eq!(star_one("initial state: #..#.#..##......###...###
+        assert_eq!(
+            star_one(
+                "initial state: #..#.#..##......###...###
 
 ...## => #
 ..#.. => #
@@ -31,7 +38,10 @@ mod tests {
 ##.## => #
 ###.. => #
 ###.# => #
-####. => #"), 1)
+####. => #"
+            ),
+            1
+        )
     }
 
     #[test]
