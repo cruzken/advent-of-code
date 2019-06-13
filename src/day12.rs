c6821df86bdfce45261885f61f16ac1c4cba19a7
Ken Cruz <ken@kencruz.ca>
2019-06-13 12:06:34 -0400

    day12::star_one made first generation

--- src/day12.rs
+++ src/day12.rs
@@ -4,8 +4,63 @@ use std::collections::HashMap;
 #[allow(unused_variables)]
 pub fn star_one(input: &str) -> i64 {
     let state = initial_state(input);
-    let patterns: HashMap<String, String> = input.lines().skip(2).map(|x| parse_pattern(x)).collect();
-    println!("{:?}", patterns);
+    let patterns: HashMap<String, String> =
+        input.lines().skip(2).map(|x| parse_pattern(x)).collect();
+    println!("{:?}", &state);
+    let mut next_gen = String::new();
+    for (i, val) in state.clone().into_iter().enumerate() {
+        let mut base = String::new();
+        match i {
+            0 => {
+                base.push_str("..");
+                base.push(state[i]);
+                base.push(state[i + 1]);
+                base.push(state[i + 2]);
+            }
+            1 => {
+                base.push_str(".");
+                base.push(state[i - 1]);
+                base.push(state[i]);
+                base.push(state[i + 1]);
+                base.push(state[i + 2]);
+            }
+            x if x == state.len() - 1 => {
+                base.push(state[i - 2]);
+                base.push(state[i - 1]);
+                base.push(state[i]);
+                base.push_str("..");
+            }
+            x if x == state.len() - 2 => {
+                base.push(state[i - 2]);
+                base.push(state[i - 1]);
+                base.push(state[i]);
+                base.push(state[i + 1]);
+                base.push_str(".");
+            }
+            _ => {
+                base.push(state[i - 2]);
+                base.push(state[i - 1]);
+                base.push(state[i]);
+                base.push(state[i + 1]);
+                base.push(state[i + 2]);
+            }
+        }
+
+        println!("{}: {}", i, base);
+
+        match patterns.get(&base) {
+            Some(x) => {
+                println!("Has match");
+                next_gen.push('#');
+            },
+            None => {
+                println!("No match");
+                next_gen.push('.');
+            },
+        }
+    }
+
+    println!("{}", next_gen);
     0
 }
 
@@ -16,13 +71,23 @@ pub fn star_two(input: &str) -> i64 {
 }
 
 fn parse_pattern(input: &str) -> (String, String) {
-    let parsed: Vec<String> = input.replace(" => ", " ").split_whitespace().map(|x| x.to_string()).collect();
+    let parsed: Vec<String> = input
+        .replace(" => ", " ")
+        .split_whitespace()
+        .map(|x| x.to_string())
+        .collect();
 
     (parsed[0].clone(), parsed[1].clone())
 }
 
-fn initial_state(input: &str) -> String {
-    input.lines().nth(0).unwrap().replace("initial state: ", "")
+fn initial_state(input: &str) -> Vec<char> {
+    input
+        .lines()
+        .nth(0)
+        .unwrap()
+        .replace("initial state: ", "")
+        .chars()
+        .collect::<Vec<char>>()
 }
 
 #[cfg(test)]
