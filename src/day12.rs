4721f9e2cf52f53e4671bcd85254ec85218a3aef
Ken Cruz <ken@kencruz.ca>
2019-06-17 01:40:26 -0400

    day12 generation loop

--- src/day12.rs
+++ src/day12.rs
@@ -2,66 +2,22 @@ use std::collections::HashMap;
 
 #[allow(dead_code)]
 #[allow(unused_variables)]
-pub fn star_one(input: &str) -> i64 {
-    let state = initial_state(input);
-    let patterns: HashMap<String, String> =
+pub fn star_one(input: &str) -> usize {
+    let mut old_gen = initial_state(input);
+    let mut sum = old_gen.chars().filter(|x| *x == '#').count();
+    let patterns: HashMap<String, char> =
         input.lines().skip(2).map(|x| parse_pattern(x)).collect();
-    println!("{:?}", &state);
-    let mut next_gen = String::new();
-    for (i, val) in state.clone().into_iter().enumerate() {
-        let mut base = String::new();
-        match i {
-            0 => {
-                base.push_str("..");
-                base.push(state[i]);
-                base.push(state[i + 1]);
-                base.push(state[i + 2]);
-            }
-            1 => {
-                base.push_str(".");
-                base.push(state[i - 1]);
-                base.push(state[i]);
-                base.push(state[i + 1]);
-                base.push(state[i + 2]);
-            }
-            x if x == state.len() - 1 => {
-                base.push(state[i - 2]);
-                base.push(state[i - 1]);
-                base.push(state[i]);
-                base.push_str("..");
-            }
-            x if x == state.len() - 2 => {
-                base.push(state[i - 2]);
-                base.push(state[i - 1]);
-                base.push(state[i]);
-                base.push(state[i + 1]);
-                base.push_str(".");
-            }
-            _ => {
-                base.push(state[i - 2]);
-                base.push(state[i - 1]);
-                base.push(state[i]);
-                base.push(state[i + 1]);
-                base.push(state[i + 2]);
-            }
-        }
 
-        println!("{}: {}", i, base);
+    let mut next_gen: String;
 
-        match patterns.get(&base) {
-            Some(x) => {
-                println!("Has match");
-                next_gen.push('#');
-            },
-            None => {
-                println!("No match");
-                next_gen.push('.');
-            },
-        }
+    println!("{} pots: {}", old_gen, sum);
+    for i in 0..20 {
+        next_gen = proceed_generation(&old_gen, &patterns);
+        old_gen = next_gen.clone();
+        sum += old_gen.chars().filter(|x| *x == '#').count();
+        println!("{}: {} pots: {}", i + 1, next_gen, sum);
     }
-
-    println!("{}", next_gen);
-    0
+    sum
 }
 
 #[allow(dead_code)]
@@ -70,24 +26,106 @@ pub fn star_two(input: &str) -> i64 {
     0
 }
 
-fn parse_pattern(input: &str) -> (String, String) {
+fn padded(input: &Vec<char>) -> Vec<char> {
+    let mut prefix = 4;
+    for item in input.into_iter() {
+        if *item == '#' || prefix == 0 {
+            break;
+        }
+        prefix -= 1;
+    }
+
+    let mut suffix = 4;
+    for item in input.into_iter().rev() {
+        if *item == '#' || suffix == 0 {
+            break;
+        }
+        suffix -= 1;
+    }
+
+    let mut output: Vec<char> = Vec::new();
+    let mut pre_vec = vec!['.'; prefix];
+    let mut suf_vec = vec!['.'; suffix];
+
+    output.append(&mut pre_vec);
+    output.append(&mut input.clone());
+    output.append(&mut suf_vec);
+    output
+}
+
+fn proceed_generation(input: &str, patterns: &HashMap<String, char>) -> String {
+    let padded_gen = padded(&input.chars().collect::<Vec<char>>());
+
+    let mut next_gen = String::new();
+    for i in 0..padded_gen.len() {
+        let mut base = String::new();
+        match i {
+            0 => {
+                base.push_str("..");
+                base.push(padded_gen[i]);
+                base.push(padded_gen[i + 1]);
+                base.push(padded_gen[i + 2]);
+            }
+            1 => {
+                base.push_str(".");
+                base.push(padded_gen[i - 1]);
+                base.push(padded_gen[i]);
+                base.push(padded_gen[i + 1]);
+                base.push(padded_gen[i + 2]);
+            }
+            x if x == padded_gen.len() - 1 => {
+                base.push(padded_gen[i - 2]);
+                base.push(padded_gen[i - 1]);
+                base.push(padded_gen[i]);
+                base.push_str("..");
+            }
+            x if x == padded_gen.len() - 2 => {
+                base.push(padded_gen[i - 2]);
+                base.push(padded_gen[i - 1]);
+                base.push(padded_gen[i]);
+                base.push(padded_gen[i + 1]);
+                base.push_str(".");
+            }
+            _ => {
+                base.push(padded_gen[i - 2]);
+                base.push(padded_gen[i - 1]);
+                base.push(padded_gen[i]);
+                base.push(padded_gen[i + 1]);
+                base.push(padded_gen[i + 2]);
+            }
+        }
+
+        match patterns.get(&base) {
+            Some(x) => {
+                next_gen.push(*x);
+            }
+            None => {
+                next_gen.push('.');
+            }
+        }
+    }
+
+    next_gen
+}
+
+fn parse_pattern(input: &str) -> (String, char) {
     let parsed: Vec<String> = input
         .replace(" => ", " ")
         .split_whitespace()
         .map(|x| x.to_string())
         .collect();
 
-    (parsed[0].clone(), parsed[1].clone())
+    (parsed[0].clone(), parsed[1].clone().chars().nth(0).unwrap())
 }
 
-fn initial_state(input: &str) -> Vec<char> {
+fn initial_state(input: &str) -> String  {
     input
         .lines()
         .nth(0)
         .unwrap()
         .replace("initial state: ", "")
         .chars()
-        .collect::<Vec<char>>()
+        .collect()
 }
 
 #[cfg(test)]
